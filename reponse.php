3bb955d92093ea35d390b9abcb98c977c588157e
OussAtmo <oussama.smaoune@atmosud.org>
2024-05-16 15:43:02 +0100

    add

--- reponse.php
+++ reponse.php
@@ -13,17 +13,28 @@
         <p>
         <?php
 
-            $reponseSexe = isset($_GET['reponseSexe']) ? $_GET['reponseSexe'] : 'Non spécifié';
-            $reponseBE = isset($_GET['reponseBE']) ? $_GET['reponseBE'] : 'Non spécifié';
-            $reponsesA = isset($_GET['reponseA']) ? $_GET['reponseA'] : 'Non spécifié';
-            echo "Vous êtes un(e) : " . htmlspecialchars($reponseSexe) . "<br>" . "<br>";
-            echo "Vous avez répondu : " . htmlspecialchars($reponseBE). "<br>" . "<br>";
-            echo "Vous avez fait : " . htmlspecialchars($reponsesA); ?>
+                $reponseSexe = isset($_GET['reponseSexe']) ? $_GET['reponseSexe'] : 'Non spécifié';
+                $r1 = isset($_GET['r1']) ? $_GET['r1'] : 'Non spécifié';
+                $r2 = isset($_GET['r2']) ? $_GET['r2'] : 'Non spécifié';
+                $r3 = isset($_GET['r3']) ? $_GET['r3'] : 'Non spécifié';
+                $r4 = isset($_GET['r4']) ? $_GET['r4'] : 'Non spécifié';
+                $r5 = isset($_GET['r5']) ? $_GET['r5'] : 'Non spécifié';
+                $r6 = isset($_GET['r6']) ? $_GET['r6'] : 'Non spécifié';
+                $r7 = isset($_GET['r7']) ? $_GET['r7'] : 'Non spécifié';
+                $r8 = isset($_GET['r8']) ? $_GET['r8'] : 'Non spécifié';
+                $r9 = isset($_GET['r9']) ? $_GET['r9'] : 'Non spécifié';
+                $r10 = isset($_GET['r10']) ? $_GET['r10'] : 'Non spécifié';
+                $r11 = isset($_GET['r11']) ? $_GET['r11'] : 'Non spécifié';
+                $r12 = isset($_GET['r12']) ? $_GET['r12'] : 'Non spécifié';
+                $r13 = isset($_GET['r13']) ? $_GET['r13'] : 'Non spécifié';
+                $r14 = isset($_GET['r14']) ? $_GET['r14'] : 'Non spécifié';
+                $r15 = isset($_GET['r15']) ? $_GET['r15'] : 'Non spécifié';
+                $r16 = isset($_GET['r16']) ? $_GET['r16'] : 'Non spécifié';
+                $r17 = isset($_GET['r17']) ? $_GET['r17'] : 'Non spécifié'; ?>
 
         </p>
             <p>La temperature est de <?php $pythonscript_temp = "Read_data_temp.py"; $output_temp = shell_exec('python ' . $pythonscript_temp ); echo $output_temp; // attention STRING not FLOAT ?> °C</p>
             <p>Le taux d'humidité est de <?php $pythonscript_hum = "Read_data_hum.py"; $output_hum = shell_exec('python ' . $pythonscript_hum ); echo $output_hum; // attention STRING not FLOAT?> %</p>
-            <p>La temperature est de <?php $pythonscript_press = "Read_data_press.py"; $output_press = shell_exec('python ' . $pythonscript_press ); echo $output_press; ?> hPa</p>
             
             
             <?php 
@@ -38,8 +49,6 @@
             
             date_default_timezone_set('Europe/Paris'); 
             $t = time(); $fullDate = date("Y-m-d H:i:s", $t); 
-            $r= $_GET["reponseBE"]; $rs= $_GET["reponseSexe"]; 
-            $reponsesA = isset($_GET['reponseA']) ? explode(',', $_GET['reponseA']) : []; $reponsesAC = implode(',', $reponsesA);
             $press = round($output_press); 
             $myfile = fopen("/sys/class/thermal/thermal_zone0/temp", "r"); 
             $temp_core = fread($myfile,filesize("/sys/class/thermal/thermal_zone0/temp")); 
@@ -63,7 +72,24 @@
                 'pm1' => floatval($output_PM1),
                 'pm25' => floatval($output_PM25),
                 'pm10' => floatval($output_PM10),
-                'genre' => $rs
+                'genre' => $reponseSexe,
+                'r1' => $r1,
+                'r2' => $r2,
+                'r3' => $r3,
+                'r4' => $r4,
+                'r5' => $r5,
+                'r6' => $r6,
+                'r7' => $r7,
+                'r8' => $r8,
+                'r9' => $r9,
+                'r10' => $r10,
+                'r11' => $r11,
+                'r12' => $r12,
+                'r13' => $r13,
+                'r14' => $r14,
+                'r15' => $r15,
+                'r16' => $r16,
+                'r17' => $r17
             );
             //$data_CNRS = json_encode($data_CNRS);
             $client = new \GuzzleHttp\Client();   
