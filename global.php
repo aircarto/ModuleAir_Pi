cdd2faba9414b6799ee0e643f65b2743cc9a346a
OussAtmo <OussAtmo@github.com>
2024-05-17 13:46:09 +0100

    add gitignore

--- global.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('DEVICE_ID', 'CNRS_1');
-file_put_contents('/var/www/html/ModuleAir_Pi/device_id.txt', DEVICE_ID); ?>
--- reponse.php
+++ reponse.php
@@ -55,7 +55,8 @@
             fclose($myfile); $temp_core = $temp_core/1000;  
 
             //Connexion to PSQL database to insert data locally
-            $conn = pg_connect("host=localhost dbname=cnrs user=airlab_test password=123plouf"); $query = "INSERT INTO reponses (
+            $conn = pg_connect("host=localhost dbname=cnrs user=airlab_test password=123plouf"); 
+            $query = "INSERT INTO reponses (
                 temperature, humidity, date_reponse, pressure, device_id, temprature_core, pm1, pm25, pm10, sexe,
                 r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30) VALUES (
                 '$output_temp', '$output_hum', '$fullDate', '$press', '".DEVICE_ID."', '$temp_core', '$output_PM1', '$output_PM25', '$output_PM10', '$reponseSexe', 
