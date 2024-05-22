7067631fbd18dcabd4d6bd04ca29b7dd269ecb87
Paul Vuarambon <48589644+PaulVua@users.noreply.github.com>
2024-05-22 11:03:59 +0200

    Update install_influx_docker.sh

--- install_influx_docker.sh
+++ install_influx_docker.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-sudo docker pull influxdb:latest
+sudo docker pull influxdb:2.7.6
 
-docker run -d \
-  --name influxdb \
-  -p 8087:8087 \
-  -v $PWD/influxdb:/var/lib/influxdb2 \
-  influxdb:latest
+sudo docker run \
+ --name influxdb2 \
+ --publish 8086:8086 \
+ --mount type=volume,source=influxdb2-data,target=/var/lib/influxdb2 \
+ --mount type=volume,source=influxdb2-config,target=/etc/influxdb2 \
+ --env DOCKER_INFLUXDB_INIT_MODE=setup \
+ --env DOCKER_INFLUXDB_INIT_USERNAME=OussAtmo \
+ --env DOCKER_INFLUXDB_INIT_PASSWORD=123plouf \
+ --env DOCKER_INFLUXDB_INIT_ORG=AC \
+ --env DOCKER_INFLUXDB_INIT_BUCKET=CNRS \
+ influxdb:2
+
