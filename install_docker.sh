0beed9db8ba44bc5078734dc441e4f042be1c725
Paul Vuarambon <48589644+PaulVua@users.noreply.github.com>
2024-05-22 10:37:34 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add Docker's official GPG key:
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
@@ -14,4 +14,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
