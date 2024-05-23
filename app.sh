957280ad71f18f34f20f10f2ac5db9727a2d27c4
PaulVua <paulvuarambon@gmail.com>
2024-05-23 12:12:52 +0200

    tests

--- app.sh
+++ app.sh
@@ -9,5 +9,7 @@ pkill chromium
 sleep 5
 
 # Version fullscreen: 
-chromium-browser --start-fullscreen http://localhost --disable-pinch --no-user-gesture-required --overscroll-history-navigation=0 --disable-infobars --disable-restore-session-state --noerrdialogs --incognito
+#chromium-browser --start-fullscreen http://localhost --disable-pinch --no-user-gesture-required --overscroll-history-navigation=0 --disable-infobars --disable-restore-session-state --noerrdialogs --incognito
 
+# Version kios: 
+chromium-browser --kiosk --disable-infobars --disable-restore-session-state --noerrdialogs --incognito http://localhost
